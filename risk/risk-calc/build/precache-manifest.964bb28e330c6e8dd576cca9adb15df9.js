026fed8468397ec371d96535d2122bd38b7f2fb9
TheLoneWanderer4 <aminsennour@gmail.com>
2019-06-15 16:51:11 -0500

    update

---
+++ risk/risk-calc/build/precache-manifest.964bb28e330c6e8dd576cca9adb15df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865d0aed90755a502f75fe619ff65531",
+    "url": "/risk-calc/index.html"
+  },
+  {
+    "revision": "2a6d02acc19967f006e9",
+    "url": "/risk-calc/static/css/main.8125ee64.chunk.css"
+  },
+  {
+    "revision": "fd7940a00b1864ff1d6d",
+    "url": "/risk-calc/static/js/2.4baf41ce.chunk.js"
+  },
+  {
+    "revision": "2a6d02acc19967f006e9",
+    "url": "/risk-calc/static/js/main.8d8773e3.chunk.js"
+  },
+  {
+    "revision": "6be74cd876049bd8fa8d",
+    "url": "/risk-calc/static/js/runtime~main.019b60f4.js"
+  }
+]);
