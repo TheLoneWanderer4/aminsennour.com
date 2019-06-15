def37843aa9634bee055c8c3cbda7566e99db9fd
TheLoneWanderer4 <aminsennour@gmail.com>
2019-06-15 16:31:27 -0500

    Updates

---
+++ precache-manifest.1dc05ad0bd6ebd46b9cf12f6eb697fe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c429e7f1df30cb87ddad3b41c8d333",
+    "url": "/risk/index.html"
+  },
+  {
+    "revision": "11a963c2f288dcf24f91",
+    "url": "/risk/static/css/main.8125ee64.chunk.css"
+  },
+  {
+    "revision": "1cddc1885353f72bc7ef",
+    "url": "/risk/static/js/2.91a388e4.chunk.js"
+  },
+  {
+    "revision": "11a963c2f288dcf24f91",
+    "url": "/risk/static/js/main.d92a017f.chunk.js"
+  },
+  {
+    "revision": "43e9ff249eca056654c7",
+    "url": "/risk/static/js/runtime~main.5f471766.js"
+  }
+]);
